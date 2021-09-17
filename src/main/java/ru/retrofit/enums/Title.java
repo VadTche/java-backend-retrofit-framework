8fe18e77834360761faa380fbc34639c924ad5ca
VadTche <pismonos@gmail.com>
2021-09-17 22:10:00 +0300

    Lesson5

--- src/main/java/ru/retrofit/enums/Title.java
+++ src/main/java/ru/retrofit/enums/Title.java
@@ -5,14 +5,17 @@ import lombok.Getter;
 public enum Title {
 
     LATIN("Latin"),
-    CYRILLIC("Cyrillic"),
-    CHINESE("Chinese"),
-    ONE_SYMBOL("1 symbol"),
-    TEN_SYMBOLS("10 symbols"),
-    ONE_HUNDRED_SYMBOLS("100 symbols"),
-    BILLION_SYMBOLS("Billion symbols"),
-    EMPTY("Empty"),
-    NULL("Null");
+    CYRILLIC("Кириллица"),
+    CHINESE("汉字"),
+    NON_LETTERS("#$%@^&"),
+    ONE_SYMBOL("F"),
+    ONE_HUNDRED_SYMBOLS("100symbols100symbols100symbols100symbols100symbols" +
+            "100symbols100symbols100symbols 100symbols100symbols"),
+    ONE_HUNDRED_SIXTY_SYMBOLS("160symbols160symbols160symbols160symbols160symbols160symbols160symbols160symbols" +
+            "160symbols160symbols160symbols160symbols160symbols160symbols160symbols160symbols"),
+    EMPTY(""),
+    SPACE(" "),
+    NULL(null);
 
     @Getter
     private String title;
