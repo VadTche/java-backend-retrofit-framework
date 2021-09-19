f223c0841380d1ab301a113b26e09ed98a0a8822
VadTche <pismonos@gmail.com>
2021-09-19 22:08:09 +0300

    Lesson5

--- src/test/java/ru/retrofit/tests/GetProductTests.java
+++ src/test/java/ru/retrofit/tests/GetProductTests.java
@@ -1,4 +1,25 @@
 package ru.retrofit.tests;
 
-public class GetProductTests {
+import org.junit.jupiter.api.Test;
+import retrofit2.Response;
+import ru.retrofit.dto.Product;
+
+import java.io.IOException;
+
+public class GetProductTests extends BaseTest{
+
+    Integer status;
+
+    @Test
+    void GetAllProductsTest() throws IOException {
+
+        Response<Product> response = productService
+                .getProducts()
+                .execute();
+        try {
+            System.out.println(response.body().getId());
+        } catch (NullPointerException e) {
+            System.out.println(response.errorBody().string());
+        }
+    }
 }
