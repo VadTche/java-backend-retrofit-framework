b5b01df7cc5569664de532d1164c3e597dab9151
VadTche <pismonos@gmail.com>
2021-09-21 21:48:08 +0300

    Lesson6

--- src/test/java/ru/retrofit/tests/BaseTest.java
+++ src/test/java/ru/retrofit/tests/BaseTest.java
@@ -1,10 +1,15 @@
 package ru.retrofit.tests;
 
 import com.github.javafaker.Faker;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import retrofit2.Retrofit;
+import ru.retrofit.db.dao.CategoriesMapper;
+import ru.retrofit.db.dao.ProductsMapper;
+import ru.retrofit.db.model.Categories;
 import ru.retrofit.service.CategoryService;
 import ru.retrofit.service.ProductService;
+import ru.retrofit.utils.DbUtils;
 import ru.retrofit.utils.RetrofitUtils;
 
 public class BaseTest {
@@ -12,12 +17,27 @@ public class BaseTest {
     static CategoryService categoryService;
     static ProductService productService;
     static Faker faker;
+    static ProductsMapper productsMapper;
+    static CategoriesMapper categoriesMapper;
+    static Categories testCategory;
 
-    @BeforeAll //создание клиента
+    @BeforeAll
     static void beforeAll() {
         retrofit = RetrofitUtils.getRetrofit();
         categoryService = retrofit.create(CategoryService.class);
         productService = retrofit.create(ProductService.class);
         faker = new Faker();
+        productsMapper = DbUtils.getProductsMapper();
+        categoriesMapper = DbUtils.getCategoriesMapper();
+
+        String quote = faker.backToTheFuture().quote();
+        //создаем новую категорию
+        testCategory = DbUtils.getNewTestCategory(quote);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        DbUtils.deleteAllProductsWithTheCategory(testCategory.getId());
+        categoriesMapper.deleteByPrimaryKey(testCategory.getId());
     }
 }
